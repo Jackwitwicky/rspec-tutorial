1c2bf3503e3090cd9536273ccca922dded87238e
Jackson Kiarie <jacknkiarie@gmail.com>
2021-09-30 10:27:01 +0300

    ADD predicate matchers

--- spec/expectation_matchers.rb
+++ spec/expectation_matchers.rb
@@ -168,4 +168,46 @@ describe 'Expectation Matchers' do
       end
     end
   end
+
+  describe 'predicate matchers' do
+    it 'will match be_* to custom methods ending in ?' do
+      # drops "be_", adds "?" to end, calls method on object
+      # can use these when methods end in "?", require no arguments
+
+      # with built-in methods
+      expect([]).to be_empty # [].empty?
+      expect(1).to be_integer # 1.integer?
+      expect(0).to be_zero # 0.zero?
+      expect(1).to be_nonzero # 1.nonzero?
+      expect(1).to be_odd # 1.odd?
+      expect(2).to be_even # 2.even?
+
+      # with custom methods
+      class Product
+        def visible?; true end
+      end
+      product = Product.new
+
+      expect(product).to be_visible # product.visible?
+      expect(product.visible?).to be true
+    end
+
+    it 'will match have_* to custom methods like has_*?' do
+      # changes "have_" to "has_", adds "?" to end calls method on object
+
+      # with built-in methods
+      hash = { a: 1, b: 2 }
+      expect(hash).to have_key(:a) # hash.has_key?
+      expect(hash).to have_value(2) # hash.has_value?
+
+      # with custom methods
+      class Customer
+        def has_pending_order?; true; end
+      end
+
+      customer = Customer.new
+      expect(customer).to have_pending_order # customer.has_pending_order?
+      expect(customer.has_pending_order?).to be true
+    end
+  end
 end
